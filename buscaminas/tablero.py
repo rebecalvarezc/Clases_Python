426a427d0bdb89989d2fb90087704431716cfb71
portilloronny6 <portilloronny6@gmail.com>
2021-09-21 10:47:46 -0400

    Actualizaciones parciales de las asignaciones referentes al ejercicio del buscaminas

--- buscaminas/tablero.py
+++ buscaminas/tablero.py
@@ -1,3 +1,21 @@
 # En este script de Python deberás definir el tablero, y los movimientos de la ficha del jugador en el tablero.
-# Puedes hacerlo con clases (recomendado) e implementando sus métodos o directamente con funciones.
+# Puedes hacerlo con clases e implementando sus métodos (recomendado) o directamente con funciones.
+# En este archivo te dejaré los respectivos TODOs para las cosas que tienes que hacer.
 
+# TODO: Escribir una función que permita crear un tablero de buscaminas.
+#  Esto básicamente consiste en proporcionar una cantidad de filas y columnas para crear una matriz con esas
+#  dimensiones. Además, también se le debe dar la posibilidad de rellenar el tablero con un valor determinado.
+
+# TODO: Escribir una función que muestre el tablero.
+#  El tablero creado anteriormente estará almacenado en una variable, por lo tanto, la razón de ser de esta función
+#  será mostrar dicho tablero.
+
+# TODO: Escribir una función que permita insertar las minas en el tablero y guardar las coordenadas de las minas.
+#  Esta función deberá recibir el tablero, sus filas y columnas y la cantidad de minas que se van a insertar en el
+#  tablero. Para representar una mina usaremos el número 9. Esta función deberá retornar el tablero con las minas
+#  y una lista con las coordenadas de las minas.
+
+# TODO: Escribir una función para mostrar el tablero con las minas en la consola.
+#  En esta función mostraremos 2 tableros, un tablero que llamaremos visible (que es el que verá el usuario)
+#  en donde cada posición será un guion "-" y otro tablero que llamaremos oculto, que rellenaremos con ceros (0)
+#  y es en ese tablero oculto donde se colocarán las minas.
