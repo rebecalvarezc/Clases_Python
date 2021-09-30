4ea57ef192c2c254982d628247bfb4842f171d89
rebecalvarezc <realce@alumni.uv.es>
2021-09-30 16:13:20 +0200

    csv module

--- archivos/movies.py
+++ archivos/movies.py
@@ -1,3 +1,5 @@
+import csv
+
 movies = [
     {'name': 'Jaws', 'director': 'Spielberg'},
     {'name': 'Silence of the Lambs', 'director': 'Pepito'},
@@ -6,32 +8,35 @@ movies = [
 ]
 
 
-def write_to_file(file_name: str, database: dict):
-    key = {','.join(info) for info in [movie.keys() for movie in database]}
-    values = [','.join(info) for info in [movie.values() for movie in database]]
+# def write_to_file(file_name: str):
+#     data = open(file_name, 'w')
+#     data.write('name,director \n')
+#     for movie in movies:
+#         data.write(f"{movie.get('name')},{movie.get('director')}\n")
+#     data.close()
+
+
+def write_to_file(file_name: str):
     data = open(file_name, 'w')
-    for i in key:
-        data.write(i + '\n')
-    for j in values:
-        data.write(j + '\n')
+    movie_file = csv.writer(data)
+    movie_file.writerows([movie.values() for movie in movies])
     data.close()
 
 
-# write_to_file('fav_movies.txt', movies)
+#write_to_file('fav_movies.txt')
+
 
 def human_print(file_name: str):
     data = open(file_name, 'r')
-    database = [item.strip() for item in data.readlines()[1:]]
+    movie_file = csv.reader(data)
+    # database = [item.strip() for item in data.readlines()[1:]]
     data.close()
+    print(movie_file)
+    # for line in movie_file:
+    #     movie_data = line.split(',')
+    #     movie_name = movie_data[0].capitalize()
+    #     movie_director = movie_data[1].capitalize()
+    #
+    #     print(f'The movie {movie_name} was directed by {movie_director}')
 
-    for line in database:
-        movie_data = line.split(',')
-        movie_name = movie_data[0].capitalize()
-        movie_director = movie_data[1].capitalize()
-
-        print(f'The movie {movie_name} was directed by {movie_director}')
-
-
-
-human_print('fav_movies')
-
+human_print('fav_movies.txt')
