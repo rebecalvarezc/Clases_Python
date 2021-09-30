d3941f02f2dbf14762c2cc72ccd80ac4f496b192
rebecalvarezc <realce@alumni.uv.es>
2021-09-30 21:09:19 +0200

    delete function bug fixed

--- libros/utils/database.py
+++ libros/utils/database.py
@@ -10,12 +10,10 @@ def add_book(name: str, author: str):
 
 
 def delete_book(name: str):
-    i = 0
-    for book in books:
-        if book.get('name') == name:
-            books.pop(i)
-        else:
-            i += 1
+    global books
+    books = [book for book in books if
+             book.get('name') != name]  # variable creada dentro de la función que itera sobre ella misma
+    # python asume que es una variable local, por lo que necesitamos ese 'global'
 
 
 def book_status(name: str):
@@ -23,4 +21,5 @@ def book_status(name: str):
         if book.get('name') == name:
             book['status'] = True
         else:
-            print('Ese libro no se encuentra en la lista.')
+            print(f'\nEl libro {name} no se encuentra en la base de datos.')
+            break
