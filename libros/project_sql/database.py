659c484c4c4da29d39d311e11ea1ba06442d5567
portilloronny6 <portilloronny6@gmail.com>
2021-10-06 19:32:33 -0400

    refactoring a little of app_sql.py and relatives

--- libros/project_sql/database.py
+++ libros/project_sql/database.py
@@ -4,19 +4,15 @@ from libros.project_sql.database_queries import *
 
 STATUS = {True: 'Leído', False: 'No leído'}
 
-database_name = 'rebeca_library.db'
+connection = sqlite3.connect('rebeca_library.db')
 
 
-def connect():
-    return sqlite3.connect(database_name)
-
-
-def create_library(connection):
+def create_library():
     with connection:
         connection.execute(CREATE_TABLE)
 
 
-def add_book(connection, title: str, author: str, status: bool = STATUS[False]):
+def add_book(title: str, author: str, status: bool = STATUS[False]):
     with connection:
         book_list = connection.execute(EXISTING_BOOKS).fetchall()
         if (title, author) not in book_list:
@@ -26,17 +22,17 @@ def add_book(connection, title: str, author: str, status: bool = STATUS[False]):
             return False
 
 
-def show_books(connection) -> list[tuple]:
+def show_books() -> list[tuple]:
     with connection:
         return connection.execute(SHOW_ALL_BOOKS).fetchall()
 
 
-def change_book_status(connection, book_id: int):
+def change_book_status(book_id: int):
     with connection:
         connection.execute(READ_BOOK, (STATUS[True], book_id))
 
 
-def remove_book(connection, book_id: int):
+def remove_book(book_id: int):
     with connection:
         existing_ids = connection.execute(EXISTING_IDS).fetchall()
         if (book_id,) in existing_ids:
