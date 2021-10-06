0e88d8f7cddd42d187b4fe61c6915e56a2bdd867
rebecalvarezc <realce@alumni.uv.es>
2021-10-07 00:25:47 +0200

    formatting

--- libros/project_sql/app_sql.py
+++ libros/project_sql/app_sql.py
@@ -23,10 +23,12 @@ def menu():
             title = input("Escribe el nombre del libro: ").title()
             author = input("Escribe el autor del libro: ").title()
             added = database.add_book(connection, title, author)
+
             if added:
                 print('\nSe ha agregado el libro satisfactoriamente!')
             else:
                 print('\nEl libro ya existe en la base de datos!')
+
         elif user_input == 'l':
             pprint.pprint(database.show_books(connection))
 
@@ -35,14 +37,17 @@ def menu():
             book_id = int(input('Escribe el ID del libro que has leído: '))
             database.change_book_status(connection, book_id)
             print(f'\nEl estado del libro de id: {book_id} ha cambiado a "Leído".')
+
         elif user_input == 'd':
             pprint.pprint(database.show_books(connection))
             book_id = int(input('Ingresa el ID del libro que deseas eliminar: '))
             deleted = database.remove_book(connection, book_id)
+
             if deleted:
                 print('\nOperación realizada satisfactoriamente :)')
             else:
                 print(f'\nNo se ha encontrado un libro con id {book_id}!')
+
         else:
             print("Por favor, ingresa un comando válido!")
     print('\nHasta pronto :)')
