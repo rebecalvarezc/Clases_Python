382d75fc1c6133ab39fd84c54675acb73e619185
portilloronny6 <portilloronny6@gmail.com>
2021-10-07 15:25:55 -0400

    improving imports and database.py

--- libros/project_sql/database.py
+++ libros/project_sql/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-import database_queries
+from database_queries import *
 
 STATUS = {True: 'Leído', False: 'No leído'}
 
@@ -11,7 +11,7 @@ def create_library():
     Función usada para crear la tabla de datos si no existe previamente.
     """
     with connection:
-        connection.execute(database_queries.CREATE_TABLE)
+        connection.execute(CREATE_TABLE)
 
 
 def add_book(title: str, author: str, status: bool = STATUS[False]) -> bool:
@@ -19,9 +19,9 @@ def add_book(title: str, author: str, status: bool = STATUS[False]) -> bool:
     Función usada para agregar el libro a las filas de la tabla de datos, siempre que este no exista previamente."
     """
     with connection:
-        book_list = connection.execute(database_queries.EXISTING_BOOKS, (title, author)).fetchone()
+        book_list = connection.execute(EXISTING_BOOKS, (title, author)).fetchone()
         if book_list is None:
-            connection.execute(database_queries.INSERT_BOOK, (title, author, status))
+            connection.execute(INSERT_BOOK, (title, author, status))
             return True
         return False
 
@@ -31,7 +31,7 @@ def show_books() -> list[tuple]:
     Función usada para mostrar todos los libros de la tabla de datos.
     """
     with connection:
-        return connection.execute(database_queries.SHOW_ALL_BOOKS).fetchall()
+        return connection.execute(SHOW_ALL_BOOKS).fetchall()
 
 
 def change_book_status(book_id: int) -> bool:
@@ -39,9 +39,9 @@ def change_book_status(book_id: int) -> bool:
     Función utilizada para cambiar el valor en el status del libro dado.
     """
     with connection:
-        existing_ids = connection.execute(database_queries.EXISTING_IDS).fetchall()
+        existing_ids = connection.execute(EXISTING_IDS).fetchall()
         if (book_id,) in existing_ids:
-            connection.execute(database_queries.READ_BOOK, (STATUS[True], book_id))
+            connection.execute(READ_BOOK, (STATUS[True], book_id))
             return True
         return False
 
@@ -51,8 +51,8 @@ def remove_book(book_id: int):
     Función utilizada para eliminar un libro y su información de la base de datos.
     """
     with connection:
-        existing_ids = connection.execute(database_queries.EXISTING_IDS).fetchall()
+        existing_ids = connection.execute(EXISTING_IDS).fetchall()
         if (book_id,) in existing_ids:
-            connection.execute(database_queries.DELETE_BOOK, (book_id,))
+            connection.execute(DELETE_BOOK, (book_id,))
             return True
         return False
