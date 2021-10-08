35ebd9f084ade35012668cd3253d0c5012238732
portilloronny6 <portilloronny6@gmail.com>
2021-10-08 09:22:57 -0400

    cambios minimos en los queries

--- movie_sql_programm/database_queries.py
+++ movie_sql_programm/database_queries.py
@@ -12,7 +12,7 @@ CHECK_MOVIE = "SELECT title, release_timestamp FROM movies WHERE title = ? AND r
 
 INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
 
-SHOW_MOVIES = "SELECT movie_id, title, release_timestamp FROM movies ;"
+SHOW_MOVIES = "SELECT movie_id, title, release_timestamp FROM movies;"
 
 UPCOMING_MOVIES = "SELECT movie_id, title, release_timestamp FROM movies WHERE release_timestamp > ?;"
 
@@ -20,7 +20,7 @@ MOVIES_IDS = "SELECT title FROM movies WHERE movie_id = ? LIMIT 1;"
 
 CHANGE_WATCHED_MOVIE = "UPDATE movies SET watched = 1, release_timestamp = ? WHERE movie_id = ?;"
 
-WATCHED_MOVIES = "SELECT movie_id, title, release_timestamp FROM movies WHERE watched =1;"
+WATCHED_MOVIES = "SELECT movie_id, title, release_timestamp FROM movies WHERE watched = 1;"
 
 # USER QUERIES
 
